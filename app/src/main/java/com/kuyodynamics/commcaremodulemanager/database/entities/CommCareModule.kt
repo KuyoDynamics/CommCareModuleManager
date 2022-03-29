f1846ed984c157770b2cf8e9329740c7ecdb57d8
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-29 19:37:12 +0200

    Update definition for CommCareModule entity

--- app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/entities/CommCareModule.kt
+++ app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/entities/CommCareModule.kt
@@ -1,8 +1,37 @@
 package com.kuyodynamics.commcaremodulemanager.database.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.PrimaryKey
 
+
+@Entity(
+    tableName = "commcare_module", foreignKeys = [
+        ForeignKey(
+            entity = CommCareApp::class,
+            parentColumns = ["appId"],
+            childColumns = ["appId"],
+            onDelete = CASCADE
+        )
+    ]
+)
 data class CommCareModule(
-    var id: String,
+
+    @PrimaryKey()
+    @ColumnInfo(name = "module_id")
+    var moduleId: String,
+
+    @ColumnInfo(name = "app_id")
+    var appId: String,
+
+    @ColumnInfo(name = "name")
     var name: String,
-    var appId: String
+
+    @ColumnInfo(name = "case_type")
+    var caseType: String,
+
+    @ColumnInfo(name = "case_properties")
+    var caseProperties: List<String>
 )
