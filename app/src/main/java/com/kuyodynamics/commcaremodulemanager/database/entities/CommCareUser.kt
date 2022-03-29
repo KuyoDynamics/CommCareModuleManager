e5461b021a4e142dcf8a1a03c26f8399393447b8
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-29 21:46:01 +0200

    Add CommCareUSer Entity

---
+++ app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/entities/CommCareUser.kt
@@ -0,0 +1,33 @@
+package com.kuyodynamics.commcaremodulemanager.database.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "commcare_user")
+data class CommCareUser(
+    @PrimaryKey()
+    @ColumnInfo(name = "id")
+    var id: String,
+
+    @ColumnInfo(name = "token")
+    var token: String,
+
+    @ColumnInfo(name = "username")
+    var username: String,
+
+    @ColumnInfo(name = "email")
+    var email: String,
+
+    @ColumnInfo(name = "last_name")
+    var lastName: String,
+
+    @ColumnInfo(name = "first_name")
+    var firstName: String,
+
+    @ColumnInfo(name = "is_admin")
+    var isAdmin: Boolean,
+
+    @ColumnInfo(name = "role")
+    var role: String
+)
