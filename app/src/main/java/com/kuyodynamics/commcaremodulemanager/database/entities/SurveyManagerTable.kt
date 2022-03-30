b6334338c7abc1db60f6c253ad139722c8699371
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-30 18:43:41 +0200

    Add SurveyManagerTable Entity

---
+++ app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/entities/SurveyManagerTable.kt
@@ -0,0 +1,44 @@
+package com.kuyodynamics.commcaremodulemanager.database.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "survey_manager_table", foreignKeys = [
+        ForeignKey(
+            entity = CommCareApp::class,
+            parentColumns = ["appId"],
+            childColumns = ["appId"],
+            onDelete = CASCADE
+        )
+    ]
+)
+data class SurveyManagerTable(
+    @PrimaryKey()
+    @ColumnInfo(name = "survey")
+    var survey: String,
+
+    @ColumnInfo(name = "app_id")
+    var appId: String,
+
+    @ColumnInfo(name = "collect")
+    var collect: String,
+
+    @ColumnInfo(name = "window")
+    var window: Int,
+
+    @ColumnInfo(name = "data_collection_round")
+    var dataCollectionRound: Int,
+
+    @ColumnInfo(name = "access_level")
+    var accessLevel: String,
+
+    @ColumnInfo(name = "last_configured", defaultValue = "CURRENT_TIMESTAMP")
+    var lastConfigured: String,
+
+    @ColumnInfo(name = "last_configured_by")
+    var lastConfiguredBy: String
+)
