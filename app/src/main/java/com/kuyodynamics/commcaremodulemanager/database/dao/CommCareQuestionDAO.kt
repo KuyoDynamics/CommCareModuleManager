712dbe9013c0b1a247f2553c7831dde792e3ecd8
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-30 16:39:23 +0200

    Add CommCareQuestion

---
+++ app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/dao/CommCareQuestionDAO.kt
@@ -0,0 +1,23 @@
+package com.kuyodynamics.commcaremodulemanager.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.kuyodynamics.commcaremodulemanager.database.entities.CommCareQuestion
+
+@Dao
+interface CommCareQuestionDAO {
+    @Insert
+    fun insert(commCareQuestion: CommCareQuestion)
+
+    @Update
+    fun update(commCareQuestion: CommCareQuestion)
+
+    @Query("SELECT * FROM commcare_question WHERE value = :questionId")
+    fun get(questionId: String): LiveData<List<CommCareQuestion>>?
+
+    @Query("SELECT * FROM commcare_question WHERE form_id = :formId")
+    fun getFormQuestions(formId: String): LiveData<List<CommCareQuestion>>?
+}
