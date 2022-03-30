400bbb2a94886b75366c63ec9c8cef4e8e115179
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-30 17:49:41 +0200

    Add UserDAO

---
+++ app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/dao/CommCareUserDAO.kt
@@ -0,0 +1,24 @@
+package com.kuyodynamics.commcaremodulemanager.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.kuyodynamics.commcaremodulemanager.database.entities.CommCareUser
+
+@Dao
+interface CommCareUserDAO {
+
+    @Insert
+    fun insert(commCareUser: CommCareUser)
+
+    @Update
+    fun update(commCareUser: CommCareUser)
+
+    @Query("SELECT * FROM commcare_user WHERE id = :id")
+    fun get(id: String): LiveData<CommCareUser>?
+
+    @Query("SELECT token FROM commcare_user WHERE id = :id")
+    fun getToken(id: String): LiveData<String>?
+
+    @Query("DELETE commcare_user WHERE id = :id")
+    fun logout(id: String)
+}
