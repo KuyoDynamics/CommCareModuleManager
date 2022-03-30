5ee24377597ba75efe313568b3f06661dd247a10
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-30 16:30:14 +0200

    Add CommCareForm

---
+++ app/src/main/java/com/kuyodynamics/commcaremodulemanager/database/dao/CommCareFormDAO.kt
@@ -0,0 +1,24 @@
+package com.kuyodynamics.commcaremodulemanager.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.kuyodynamics.commcaremodulemanager.database.entities.CommCareForm
+
+@Dao
+interface CommCareFormDAO {
+    @Insert
+    fun insert(commcareForm: CommCareForm)
+
+    @Update
+    fun update(commcareForm: CommCareForm)
+
+    @Query("SELECT * FROM commcare_form WHERE form_id = :formId")
+    fun get(formId: String): LiveData<CommCareForm>?
+
+    @Query("SELECT * FROM commcare_form WHERE module_id = :moduleId")
+    fun getModuleForms(moduleId: String): LiveData<List<CommCareForm>>?
+
+}
