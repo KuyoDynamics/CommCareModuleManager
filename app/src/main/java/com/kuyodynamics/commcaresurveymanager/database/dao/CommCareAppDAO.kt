73397cc2b0808bbbf87515edb3d9892acbc3eb55
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-04-16 17:02:51 +0200

    Retrive apps from local db and load it into the repo

--- app/src/main/java/com/kuyodynamics/commcaresurveymanager/database/dao/CommCareAppDAO.kt
+++ app/src/main/java/com/kuyodynamics/commcaresurveymanager/database/dao/CommCareAppDAO.kt
@@ -17,11 +17,11 @@ interface CommCareAppDAO {
     fun update(commcareApp: CommCareApp)
 
     @Query("SELECT * FROM commcare_app WHERE app_id = :appId")
-    fun get(appId: String): LiveData<CommCareApp>?
+    fun get(appId: String): LiveData<CommCareApp>
 
     @Query("DELETE FROM commcare_app")
     fun clear()
 
     @Query("SELECT * FROM commcare_app ORDER BY name DESC")
-    fun getAllApps(): LiveData<List<CommCareApp>>?
+    fun getAllApps(): LiveData<List<CommCareApp>>
 }
--- app/src/main/java/com/kuyodynamics/commcaresurveymanager/repository/CommCareAppRepo.kt
+++ app/src/main/java/com/kuyodynamics/commcaresurveymanager/repository/CommCareAppRepo.kt
@@ -1,6 +1,8 @@
 package com.kuyodynamics.commcaresurveymanager.repository
 
+import androidx.lifecycle.LiveData
 import com.kuyodynamics.commcaresurveymanager.database.SurveyManagerDatabase
+import com.kuyodynamics.commcaresurveymanager.database.entities.CommCareApp
 import com.kuyodynamics.commcaresurveymanager.network.CommCareNetwork
 import com.kuyodynamics.commcaresurveymanager.network.CommCareService
 import com.kuyodynamics.commcaresurveymanager.network.asDatabaseModel
@@ -9,14 +11,18 @@ import kotlinx.coroutines.withContext
 
 class CommCareAppRepo(private val database: SurveyManagerDatabase) {
 
+    // get apps from network and update local DB(room)
     suspend fun refreshApps(projectName: String){
 
         withContext(Dispatchers.IO){
             // fetch the Apps list from the network using the Retrofit service
-            val apps = CommCareNetwork.apps.getApps(projectName)
+            val appList = CommCareNetwork.apps.getApps(projectName)
             // store the fetched apps in the Room database
-            database.commCareAppDAO.insertAll(apps.asDatabaseModel())
+            database.commCareAppDAO.insertAll(appList.asDatabaseModel())
         }
-
     }
+
+    // Retrieve data from the local database as store it memory as Domain Model
+    val apps: LiveData<List<CommCareApp>> = database.commCareAppDAO.getAllApps()
+
 }
